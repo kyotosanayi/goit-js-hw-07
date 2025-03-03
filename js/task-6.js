f031481598b738499a3f44c29d748ea9b0c920fa
sumeyye_terzi <sumeyye61terzi@gmail.com>
2025-03-03 13:17:57 +0300

    task-6 is done

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,39 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("#controls input");
+const createButton = document.querySelector("#controls button[data-create]");
+const destroyButton = document.querySelector("#controls button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+let box = {
+  width: 30,
+  height: 30,
+};
+
+createButton.addEventListener("click", (event) => {
+  boxes.innerHTML = "";
+  const inputValue = Number(input.value);
+  if (inputValue < 1 || inputValue > 100) {
+    return;
+  }
+
+  box = {
+    width: 30,
+    height: 30,
+  };
+  for (let i = 0; i < inputValue; i += 1) {
+    const divbox = document.createElement("div");
+    divbox.style.width = `${box.width}px`;
+    divbox.style.height = `${box.height}px`;
+    divbox.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(divbox);
+    box.width += 10;
+    box.height += 10;
+  }
+});
+
+destroyButton.addEventListener("click", (event) => {
+  boxes.innerHTML = "";
+  input.value = "";
+});
